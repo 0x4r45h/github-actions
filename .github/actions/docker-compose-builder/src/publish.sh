6e483003e9a10db904aad9ce45e286346cd6e84f
0x4r45h <0x4r45h@gmail.com>
2024-07-20 19:01:59 +0330

    trying to build inline

--- .github/actions/docker-compose-builder/src/publish.sh
+++ .github/actions/docker-compose-builder/src/publish.sh
@@ -1,24 +1,8 @@
 PROFILES="$1"
 COMPOSE_FILES="$2"
-REPO_TOKEN="$3"
-GITHUB_REPOSITORY=$(echo "$GITHUB_REPOSITORY" | awk '{print tolower($0)}')
 
 echo "PROFILES=$PROFILES"
 echo "COMPOSE_FILES=$COMPOSE_FILES"
 
-#docker login ghcr.io -u "${GITHUB_REF}" -p "${REPO_TOKEN}"
 docker compose $COMPOSE_FILES $PROFILES build
 docker compose $COMPOSE_FILES $PROFILES push
-#docker compose "$COMPOSE_FILES" "$PROFILES" push
-#IMAGES=$(docker inspect --format='{{.Image}}' "$(docker compose ps -aq)")
-
-#echo "IMAGES: $IMAGES"
-#for IMAGE in $IMAGES; do
-#    echo "IMAGE: $IMAGE"
-#
-#    NAME=$(basename "${GITHUB_REPOSITORY}").$(docker inspect --format '{{ index .Config.Labels "name" }}' "$IMAGE")
-#    TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
-#
-#    docker tag "$IMAGE" "$TAG"
-#    docker push "$TAG"
-#done
