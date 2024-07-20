ce41c7552ddf2ae857e6d27d875058018d5bd4b1
0x4r45h <0x4r45h@gmail.com>
2024-07-20 18:13:58 +0330

    my builder

--- .github/actions/docker-compose-builder/src/publish.sh
+++ .github/actions/docker-compose-builder/src/publish.sh
@@ -8,16 +8,17 @@ echo "OVERRIDE=$OVERRIDE"
 
 docker login ghcr.io -u "${GITHUB_REF}" -p "${REPO_TOKEN}"
 
-VERSION=$VERSION docker-compose -f docker-compose.yml -f "$OVERRIDE" up --no-start --remove-orphans
-IMAGES=$(docker inspect --format='{{.Image}}' "$(docker ps -aq)")
+docker-compose -f docker-compose.yml -f "$OVERRIDE" build
+docker-compose -f docker-compose.yml -f "$OVERRIDE" push
+#IMAGES=$(docker inspect --format='{{.Image}}' "$(docker compose ps -aq)")
 
-echo "IMAGES: $IMAGES"
-for IMAGE in $IMAGES; do
-    echo "IMAGE: $IMAGE"
-    
-    NAME=$(basename "${GITHUB_REPOSITORY}").$(docker inspect --format '{{ index .Config.Labels "name" }}' "$IMAGE")
-    TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
-
-    docker tag "$IMAGE" "$TAG"
-    docker push "$TAG"
-done
+#echo "IMAGES: $IMAGES"
+#for IMAGE in $IMAGES; do
+#    echo "IMAGE: $IMAGE"
+#
+#    NAME=$(basename "${GITHUB_REPOSITORY}").$(docker inspect --format '{{ index .Config.Labels "name" }}' "$IMAGE")
+#    TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
+#
+#    docker tag "$IMAGE" "$TAG"
+#    docker push "$TAG"
+#done
